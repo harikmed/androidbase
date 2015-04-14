fe5022e2ee5283759dd43f575a92176a2472f31f
harik <hamouda.form@gmail.com>
2015-04-14 13:34:32 +0000

    creation code source pour charger le site web dans la web view

--- src/com/harik/androidbase/MainActivity.java
+++ src/com/harik/androidbase/MainActivity.java
@@ -2,14 +2,18 @@ package com.harik.androidbase;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
-
+WebView wb;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		wb=(WebView) findViewById(R.id.webView1);
+	
+		wb.loadUrl("http://www.eurosport.fr/");
+	wb.getSettings().setJavaScriptEnabled(true);
+	wb.getSettings().setDomStorageEnabled(true);
 	}
 }
