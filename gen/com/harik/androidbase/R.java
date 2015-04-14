33ea8d008a3cf31941dfb4e1cf3c524e16d7729f
harik <hamouda.form@gmail.com>
2015-04-14 13:07:41 +0000

    creation de l'interface radio(layout/ )

--- gen/com/harik/androidbase/R.java
+++ gen/com/harik/androidbase/R.java
@@ -12,6 +12,19 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int pause=0x7f020001;
+        public static final int play=0x7f020002;
+        public static final int record=0x7f020003;
+        public static final int stop=0x7f020004;
+    }
+    public static final class id {
+        public static final int LinearLayout1=0x7f060000;
+        public static final int button1=0x7f060001;
+        public static final int button2=0x7f060002;
+        public static final int button3=0x7f060005;
+        public static final int button4=0x7f060006;
+        public static final int textView1=0x7f060003;
+        public static final int webView1=0x7f060004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
